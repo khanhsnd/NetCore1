f22a1bbda7bdf20a4eb05ad2956d0c2b8d5258a0
khanhnd <duckhanh.0811@gmail.com>
2021-09-26 00:05:33 +0700

    update thêm class Extensions/ServiceExtensions.cs

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    // dùng để config , tạo thêm để đỡ viết nhiều trong file startup 
+    public static class ServiceExtensions
+    {
+        // config CORS
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+        }
+        // config IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+    }
+}
